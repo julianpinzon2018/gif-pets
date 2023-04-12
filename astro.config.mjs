36c758cbb8d3b7497553f8057cddcdebfd0c9a25
Julian Pinzon <julianpinzon2018@gmail.com>
2023-04-11 23:12:13 -0500

    as

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,11 @@
 import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/static";
 import react from "@astrojs/react";
-import partytown from "@astrojs/partytown";
 import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   output: "static",
   adapter: vercel(),
-  integrations: [
-    react(),
-    partytown(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-  ],
+  integrations: [react(), image()],
 });
